f6ace3ad31c16fc100f8b0a57d1d64b8418d9e0c
WilliamLac <William06>
2016-06-19 17:00:51 -0300

    Location working, with default set to Humaita

--- Stormy/app/src/main/java/br/com/williamlacerda/stormy/UI/DailyForecastActivity.java
+++ Stormy/app/src/main/java/br/com/williamlacerda/stormy/UI/DailyForecastActivity.java
@@ -6,23 +6,29 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.Arrays;
 
 import br.com.williamlacerda.stormy.R;
 import br.com.williamlacerda.stormy.adapters.DayAdapter;
 import br.com.williamlacerda.stormy.weather.Day;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DailyForecastActivity extends ListActivity {
 
     private Day[] mDays;
+    @BindView(R.id.locationLabel)
+    TextView mLocationLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
-
+        ButterKnife.bind(this);
         Intent intent = getIntent();
+        mLocationLabel.setText(intent.getStringExtra(MainActivity.LOCATIONNAME));
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
         mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
